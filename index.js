b6f79d149da45cf8ebc0ba685650dc0726d270dc
Aryan Gupta <96881807+aryanGupta-09@users.noreply.github.com>
2024-10-28 23:09:54 +0530

    set up expressjs app

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const expressLayouts = require("express-ejs-layouts");
+
+app.use(express.static("./assets"));
+
+// set up the view engine
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+// use express-ejs-layouts middleware
+app.use(expressLayouts);
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
+app.use("/", require("./routes"));
+
+app.listen(port, function (err) {
+    if (err) {
+        console.log(`Error in running the server: ${err}`);
+    } else {
+        console.log(`Server is running on port: ${port}`);
+    }
+});
