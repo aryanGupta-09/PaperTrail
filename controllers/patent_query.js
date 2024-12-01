50f79d74cb7974ccc9b8e19269cfb597903d387d
Aryan Gupta <96881807+aryanGupta-09@users.noreply.github.com>
2024-12-02 00:42:32 +0530

    applied soundex in patent authors

--- controllers/patent_query.js
+++ controllers/patent_query.js
@@ -3,15 +3,14 @@ const mysql = require('mysql');
 
 // Build dynamic query based on filters
 const buildDynamicQuery = (filters) => {
-    let query = 'SELECT p.patent_id, p.patent_title, p.patent_date, p.patent_year, a.author_name, ass.assignee_organization, p.patent_abstract ';
-    
+    let query = 'SELECT p.patent_id, p.patent_title, p.patent_date, p.patent_year, a.author_name, p.patent_abstract ';
+
     // Base from the patents table
     query += 'FROM patents AS p ';
-    
+
     // Define the joins based on filters
     const joins = [];
     joins.push('LEFT JOIN authors AS a ON p.author_id = a.author_id');
-    joins.push('LEFT JOIN assignees AS ass ON p.assignee_id = ass.assignee_id');
 
     // Append the joins to the query
     query += joins.join(' ');
@@ -21,9 +20,23 @@ const buildDynamicQuery = (filters) => {
     if (filters.patent_id) conditions.push(`p.patent_id = ${mysql.escape(filters.patent_id)}`);
     if (filters.patent_title) conditions.push(`p.patent_title LIKE ${mysql.escape('%' + filters.patent_title + '%')}`)
     if (filters.patent_year) conditions.push(`p.patent_year = ${mysql.escape(filters.patent_year)}`);
-    if (filters.author_name) conditions.push(`a.author_name = ${mysql.escape(filters.author_name)}`);
-    if (filters.assignee_organization) conditions.push(`ass.assignee_organization = ${mysql.escape(filters.assignee_organization)}`);
-    if (filters.patent_date) conditions.push(`p.patent_date = ${mysql.escape(filters.patent_date)}`);
+    if (filters.author_name) {
+        if (filters.author_name.trim().split(' ').length === 1) {
+            // Single-word author name
+            conditions.push(`
+          SOUNDEX(${mysql.escape(filters.author_name)}) IN (
+            SOUNDEX(SUBSTRING_INDEX(a.author_name, ' ', 1)),
+            SOUNDEX(SUBSTRING_INDEX(SUBSTRING_INDEX(a.author_name, ' ', 2), ' ', -1)),
+            SOUNDEX(SUBSTRING_INDEX(SUBSTRING_INDEX(a.author_name, ' ', 3), ' ', -1)),
+            SOUNDEX(SUBSTRING_INDEX(SUBSTRING_INDEX(a.author_name, ' ', 4), ' ', -1)),
+            SOUNDEX(SUBSTRING_INDEX(SUBSTRING_INDEX(a.author_name, ' ', 5), ' ', -1))
+          )
+        `);
+        } else {
+            // Multiple-word author name
+            conditions.push(`SOUNDEX(a.author_name) = SOUNDEX(${mysql.escape(filters.author_name)})`);
+        }
+    }
 
     // Add conditions to the WHERE clause
     if (conditions.length > 0) {
